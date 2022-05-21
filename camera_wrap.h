16d44b3d846bc86bf6427306c85d8e4194432381
bachptnk <91135899+bachptnk@users.noreply.github.com>
2022-05-21 16:39:52 +0700

    Add files via upload

---
+++ camera_wrap.h
@@ -0,0 +1,9 @@
+//STANDARD OR BASE LIBRARIES FOR RUNNING
+#pragma once
+
+#include "esp_camera.h"
+#include "Arduino.h"
+#include <string.h>
+
+extern int initCamera();
+extern esp_err_t grabImage( size_t& jpg_buf_len, uint8_t *jpg_buf);
